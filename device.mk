39a9e16a13386c2ee7f3ed39abec81ce8133224c
nazunamoe <nakashimayuki@kakao.com>
2021-05-01 12:59:38 +0900

    clover: ArrowOS Bringup

--- device.mk
+++ device.mk
@@ -61,7 +61,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-arrow    
 
 # Permissions
 PRODUCT_COPY_FILES += \
